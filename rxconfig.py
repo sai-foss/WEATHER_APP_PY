6c0224c252dc0d30f949df5929eea50adc0f026a
Sai <saibharathfoss@gmail.com>
2025-12-26 22:33:57 -0600

    https correction in config

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Flight_Analytics_App",
-    api_url="http://flight-analytics-app.onrender.com:8000",
+    api_url="https://flight-analytics-app.onrender.com:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
